5a5dbb7eb2c0e76937c5895540d61a3b726d82c6
Сергей Косолапов <serega.kosolapov2003@gmail.com>
2022-03-31 15:46:09 +0500

    Завершил 3 этап лабораторной работы

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -12,7 +12,8 @@ static void RenderSceneCB()
     glEnableVertexAttribArray(0); //координаты вершин, используемые в буфере, рассматриваются как атрибут вершины с индексом 0
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glDrawArrays(GL_POINT, 0, 1); //функция для отрисовки
+    //glDrawArrays(GL_POINT, 0, 1); функция для отрисовки
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     glDisableVertexAttribArray(0); //отключение атрибута вершины
 
     glutSwapBuffers(); //фоновый буфер меняется с буфером кадра
@@ -25,7 +26,7 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //настройка опций (двойная буферизация и буфер цвета)
     glutInitWindowSize(1024, 768);  //создание и изменения окна
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 01");
+    glutCreateWindow("Tutorial 1");
     glutDisplayFunc(RenderSceneCB); //взаимодействие с оконной системой
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //вызов цвета
 
@@ -37,9 +38,11 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    glm::vec3 Vertices[1]; //создается массив из 2 экземпляра
-    Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f); //Позиции x, y, z по 0
-
+    glm::vec3 Vertices[3]; //создается массив из 2 экземпляра
+    // Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f)Позиции x, y, z по 0
+    Vertices[0] = glm::vec3(-1.0f, 1.0f, 0.0f);
+    Vertices[1] = glm::vec3(-1.0f, -1.0f, 0.0f);
+    Vertices[2] = glm::vec3(1.0f, 0.0f, 0.0f);
 
     glGenBuffers(1, &VBO); //генерация объектов перемен. типов(Кол-во объектов и сслыка на массив)
     glBindBuffer(GL_ARRAY_BUFFER, VBO); //привязка указателя к названию цели 
